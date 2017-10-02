f0e2f3ebccffbe2c5efbd8aca7070b89d55de722
tmw98 <vjhalle1998@gmail.com>
2017-10-02 19:39:37 +0530

    Added editor_config.h and print file contents

---
+++ editor_config.h
@@ -0,0 +1,9 @@
+
+
+typedef struct editor_config {
+    int cursor_x;   //cursor's x co ordinate
+    int cursor_y;   // cursor's y co oridnate
+    int screen_row; // max number of rows that can be displayed on screen
+    int screen_col; //max number of columns that can be displayed on screen
+    int no_rows; //number of rows
+} editor_config;
--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <string.h>
-
-int maxrow = 0, maxcol = 0;
+#include <fcntl.h>
+#include <unistd.h>
+#include "editor_config.h"
 
 WINDOW *create_newwin(int height, int width, int startx, int starty) {
     WINDOW *local_win;
     local_win = newwin(height, width, starty, startx);
-    box(local_win, 0, 0);
+    // box(local_win, 0, 0);
     wrefresh(local_win);
     return local_win;
 }
@@ -18,56 +19,53 @@ void destroy_mywin(WINDOW *local_win) {
     delwin(local_win);
 }
 
-void initEditor() {
+WINDOW *init_editor(const char *filename) {
+    editor_config ec;
+    ec.cursor_x = 0;
+    ec.cursor_y = 0;
+    ec.screen_row = 0;
+    ec.screen_col = 0;
+    ec.no_rows = 1;
     initscr();
     cbreak();
     // noecho();
-    int row, col, startx, starty, height = 1, width = col, cursorx = 0, cursory = 0;
-    char mesg[] = "SHIT";
-    getmaxyx(stdscr, row, col);
+    getmaxyx(stdscr, ec.screen_row, ec.screen_col);
     // mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
     // mvprintw(row - 2, 0, "this screen has %d rows and %d columns", row, col);
-    WINDOW *first;
-    startx = 0;
-    starty = 0;
-    char ch;
+    WINDOW *win;
+    int startx, starty;
+    startx = starty = 0;
+    // char c;
     noecho();
-    first = create_newwin(height, width, startx, starty);
-    keypad(first, TRUE);
-    while((ch = getch()) != 'a') {
-        switch(ch) {
-            case KEY_UP:
-                if(height > 1) {
-                    destroy_mywin(first);
-                    first = create_newwin(--height, width, startx, starty);
-                }
-                break;
-            case KEY_DOWN:
-                if(height < row) {
-                    destroy_mywin(first);
-                    first = create_newwin(++height, width, startx, starty);
-                }
-                break;
-            case KEY_ENTER:
-                mvprintw(cursory, cursorx, "\n");
-                cursory++;
-                cursorx = 0;
-                break;
-            default:
-                mvprintw(cursory, cursorx, "%c", ch);
-                cursorx++;
-                break;
-                // mvprintw()
+    win = create_newwin(ec.screen_row - 2, ec.screen_col, startx, starty);
+    keypad(win, TRUE);
+    int fd = open(filename, O_RDONLY);
+    char ch;
+    while(read(fd, &ch, 1) != 0) {
+        mvprintw(ec.cursor_y, ec.cursor_x, "%c", ch);
+        // printw("%c", ch);
+        if(ch == 10){
+            ec.cursor_x = 0;
+            ec.cursor_y++;
         }
+        else
+            ec.cursor_x++;
     }
-    // refresh();
-    getch();
-    endwin();
+    refresh();
+    close(fd);
+    return win;
 }
 
 
 int main(int argc, char const *argv[])
 {
-    initEditor();
+    if(argc != 2) {
+        printf("Usage : ./main <filename>\n");
+        return -1;
+    }
+    WINDOW *win;
+    win = init_editor(argv[1]);
+    exit_editor(win);
+
     return 0;
 }
