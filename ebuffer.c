dfbd0fe6f998f72f45f5289ed4011b7405f72fcb
tmw98 <vjhalle1998@gmail.com>
2017-10-04 01:08:44 +0530

    initbuffer add_char_to_buffer added

--- ebuffer.c
+++ ebuffer.c
@@ -1,27 +1,32 @@
 #include "ebuffer.h"
 
 void init_buffer(ebuffer *buff) {
+
     buff = (ebuffer *) (malloc(sizeof(ebuffer)));
     buff -> prev = NULL;
     buff -> next = NULL;
-    (buff -> row_buffer).start = (char *) (calloc(1, 4096));
-    (buff -> row_buffer).cursor_ptr = start;
-    (buff -> row_buffer).end_ptr = start;
-    (buff -> row_buffer).capacity = 512;
-    (buff -> row_buffer).gap_end_ptr = start + 512;
-    (buff -> row_buffer).len = 0;
+
+    (buff -> row_buffer).buffer_size = 4096;
+    (buff -> row_buffer).buffer = (char *) (calloc(1, (buff -> row_buffer).buffer_size));
+    if(!(buff -> row_buffer).buffer)
+        return;
+    (buff -> row_buffer).cursor_ptr = (buff -> row_buffer).buffer;
+    (buff -> row_buffer).gap_end = (buff -> row_buffer).buffer + (buff -> row_buffer).buffer_size;
+    (buff -> row_buffer).gap_capacity = 512;
+    (buff -> row_buffer).gap_start = (buff -> row_buffer).buffer;
+    (buff -> row_buffer).bufend = (buff -> row_buffer).gap_end;
+    // (buff -> row_buffer).len = 0;
 }
 
-void add_char_to_buffer(ebuffer *buff, char ch) {
-    if(capacity == 0) {
+void add_char(ebuffer *buff, char ch) {
+    if((buff -> row_buffer).cursor_ptr != (buff -> row_buffer).gap_start)
+        move_gap_to_cursor_ptr(buff);
+
+    if(is_full_gap(buff))
         expand_buffer(buff);
-    }
-    *cursor_ptr = ch;
-    if(cursor_ptr == end_ptr)
-        ++end_ptr;
-    ++cursor_ptr;
-    --capacity;
-    ++len;
+
+    *((buff -> row_buffer).gap_start++) = ch;
+    *((buff -> row_buffer).cursor_ptr++);
 }
 
 void expand_buffer(ebuffer *buff) {
--- ebuffer.h
+++ ebuffer.h
@@ -1,10 +1,11 @@
 typedef struct gap_buffer {
-    char *start;
-    char *cursor_ptr;
-    char *gap_end_ptr;
-    char *end_ptr;
-    int capacity;
-    int len;
+    char *cursor_ptr;                    // location pointer into buffer
+    char *buffer;                   // start of text buffer
+    char *bufend;                   // first location outside buffer
+    char *gap_start;                 // start of gap
+    char *gap_end;
+    int buffer_size;
+    int gap_capacity;
 } gap_buffer;
 
 typedef struct list_of_buffers {
@@ -12,30 +13,8 @@ typedef struct list_of_buffers {
     gap_buffer *prev, *next;
 } ebuffer;
 
-void init_buffer(ebuffer *buff) {
-    buff = (ebuffer *) (malloc(sizeof(ebuffer)));
-    buff -> prev = NULL;
-    buff -> next = NULL;
-    (buff -> row_buffer).start = (char *) (calloc(1, 4096));
-    (buff -> row_buffer).cursor_ptr = start;
-    (buff -> row_buffer).end_ptr = start;
-    (buff -> row_buffer).capacity = 512;
-    (buff -> row_buffer).gap_end_ptr = start + 512;
-    (buff -> row_buffer).len = 0;
-}
+void init_buffer(ebuffer *buff);
 
-void add_char_to_buffer(ebuffer *buff, char ch) {
-    if(capacity == 0) {
-        expand_buffer(buff);
-    }
-    *cursor_ptr = ch;
-    if(cursor_ptr == end_ptr)
-        ++end_ptr;
-    ++cursor_ptr;
-    --capacity;
-    ++len;
-}
+void add_char(ebuffer *buff, char ch) ;
 
-void expand_buffer(ebuffer *buff) {
-
-}
+void expand_buffer(ebuffer *buff);
